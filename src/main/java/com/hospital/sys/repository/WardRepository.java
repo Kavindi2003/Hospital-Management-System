9542aebdbcdb544b64cd467038d592703e99a515
thusharinilmini00041-jpg <thusharinilmini00041@gmail.com>
2026-07-28 21:30:51 +0530

    Delete src/main/java/com/hospital/sys/repository directory

--- src/main/java/com/hospital/sys/repository/WardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hospital.sys.repository;
-
-import com.hospital.sys.entity.Ward;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WardRepository extends JpaRepository<Ward, Long> {
-}
